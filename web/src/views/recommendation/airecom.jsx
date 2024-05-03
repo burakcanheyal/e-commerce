901b03fde9f40503a31e50b7def551f5fcf1e87b
cbuyuk <cgbuyuk@gmail.com>
2024-05-03 22:48:22 +0300

    airecom added v3

---
+++ web/src/views/recommendation/airecom.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecommendationPage = () => {
+  return (
+    <div className="container">
+      <h1>AI Recommendation</h1>
+    </div>
+  );
+};
+
+export default RecommendationPage;
